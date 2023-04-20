d77fe59337ab5fff156d76dc00f68d69c8f8860c
aSouchereau <98470000+aSouchereau@users.noreply.github.com>
2023-04-20 15:10:18 -0400

    Created hero and nav

---
+++ components/HeroSectionComponent.tsx
@@ -0,0 +1,10 @@
+export default function HeroSection () {
+    return (
+        <section id='hero'>
+            <div id='hero-content'>
+                <h1>Alex Souchereau</h1>
+                <h2>Web Developer</h2>
+            </div>
+        </section>
+    )
+}
---
+++ components/NavComponent.tsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+    return <nav id='main-nav'>
+        <ul className='nav-list'>
+            <li className='nav-item'><a href='https://github.com/aSouchereau'>Github</a></li>
+            <li className='nav-item'><a href='#city-cyclery'>City Cyclery</a></li>
+            <li className='nav-item'><a href='#linkboard'>Linkboard</a></li>
+            <li className='nav-item'><a href='#mc-route-manager'>MC Route Manager</a></li>
+            <li className='nav-item'><a href='#hickory'>Hickory</a></li>
+        </ul>
+    </nav>
+}
