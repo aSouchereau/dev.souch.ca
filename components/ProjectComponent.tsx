41960362159235199c26bd593512ddeb50237230
aSouchereau <98470000+aSouchereau@users.noreply.github.com>
2023-04-21 19:10:47 -0400

    Implemented tags

--- components/ProjectComponent.tsx
+++ components/ProjectComponent.tsx
@@ -1,4 +1,5 @@
 import {Project} from "@/utils/projects";
+import ProjectTag from "@/components/TagComponent";
 
 
 export default function ProjectComponent(project: Project, light: boolean) {
@@ -6,6 +7,9 @@ export default function ProjectComponent(project: Project, light: boolean) {
         <h3 className={(light ? 'light' : '')}>{project.title}</h3>
         <h4 className={(light ? 'light' : '')}>{project.subtitle}</h4>
 
-        <p>{project.description}</p>
+        <p className={(light ? 'light' : '')}>{project.description}</p>
+        {project.tags.map((tag) => (
+            <ProjectTag text={tag} />
+        ))}
     </div>;
 }
---
+++ components/TagComponent.tsx
@@ -0,0 +1,5 @@
+export default function ProjectTag({text}: string) {
+    return <div className='tag-wrapper'>
+        <span>{text}</span>
+    </div>
+}
