8526ea825a43c8049f1a2be8a01c15e137a2b263
aSouchereau <98470000+aSouchereau@users.noreply.github.com>
2023-04-21 20:00:31 -0400

    Added skills content

--- components/SkillsSectionComponent.tsx
+++ components/SkillsSectionComponent.tsx
@@ -1,4 +1,14 @@
 export default function SkillsSection() {
-    return <>
-    </>
+    return <section id='skills'>
+       <div className='skills-wrapper'>
+           <div className='skill-content'>
+               <h2>Development</h2>
+               <p>I’m very confident in my HTML and CSS skills. I primarily work with Dotnet, Laravel, Angular, and Bootstrap. I also have experience with a variety of other tools such as Nodejs, Nextjs, Tailwind and more.</p>
+           </div>
+           <div className='skill-content'>
+               <h2>Design</h2>
+               <p>My design strengths are in typography, colour theory, ui design, and creating brand identity. I mostly use Illustrator, Indesign, Photoshop, and After Effects for graphics. And for the Web, I use figma, and I also have experience with Adobe XD.</p>
+           </div>
+       </div>
+    </section>
 }
