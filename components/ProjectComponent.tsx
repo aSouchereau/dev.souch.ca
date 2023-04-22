5f2c787cb82e977b3835c411519f36b858a4dfa8
aSouchereau <98470000+aSouchereau@users.noreply.github.com>
2023-04-21 22:27:08 -0400

    Styled Projects section

--- components/ProjectComponent.tsx
+++ components/ProjectComponent.tsx
@@ -1,15 +1,25 @@
 import {Project} from "@/utils/projects";
 import ProjectTag from "@/components/TagComponent";
+import Image from "next/image";
 
 
 export default function ProjectComponent(project: Project) {
-    return <div>
-        <h3 className='light'>{project.title}</h3>
-        <h4 className='light'>{project.subtitle}</h4>
+    return <div className='project'>
+        <div className='project-content'>
+            <h3 className='light'>{project.title}</h3>
+            <h4 className='light'>{project.subtitle}</h4>
+            <div className='tag-list'>
+                {project.tags.map((tag) => (
+                    <div key={tag}>
+                        <ProjectTag text={tag} />
+                    </div>
+                ))}
+            </div>
 
-        <p className='light'>{project.description}</p>
-        {project.tags.map((tag) => (
-            <ProjectTag text={tag} />
-        ))}
+            <p className='light'>{project.description}</p>
+        </div>
+        <div className='project-image image-wrapper'>
+            <span></span>
+        </div>
     </div>;
 }
