63c922fbd5b525c29ac6eec4bd70814d39e35af2
aSouchereau <98470000+aSouchereau@users.noreply.github.com>
2023-04-21 23:28:09 -0400

    Made nav mobile responsive

--- components/NavComponent.tsx
+++ components/NavComponent.tsx
@@ -1,5 +1,9 @@
 export default function Nav() {
     return <nav id='main-nav'>
+        <div id='hero-content-mobile'>
+            <h1 className='light'>Alex Souchereau</h1>
+            <h2 className='light'>Web Developer</h2>
+        </div>
         <ul className='nav-list'>
             <li className='nav-item'><a href='https://github.com/aSouchereau'>Github</a></li>
             <li className='nav-item'><a href='#city-cyclery'>City Cyclery</a></li>
