9473be609eea7947663b5970cb1270b460342b5b
aSouchereau <98470000+aSouchereau@users.noreply.github.com>
2023-04-21 23:30:33 -0400

    Added slug to project so links work now

--- components/ProjectComponent.tsx
+++ components/ProjectComponent.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 
 export default function ProjectComponent(project: Project) {
-    return <div className='project'>
+    return <div className='project' id={project.slug}>
         <div className='project-content'>
             <h3 className='light'>{project.title}</h3>
             <h4 className='light'>{project.subtitle}</h4>
