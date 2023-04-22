c68d03769d784d12194db3c11c181e02e93346b3
aSouchereau <98470000+aSouchereau@users.noreply.github.com>
2023-04-21 22:15:29 -0400

    Added about section content

--- components/AboutSectionComponent.tsx
+++ components/AboutSectionComponent.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
 export default function AboutSection() {
     return <section id='about'>
-        <div className=''>
-
+        <div className='about-item intro-paragraph'>
+            <h2 className='light'>Hey, I’m Alex. </h2>
+            <p className='light'>I’m a full stack developer with a focus on front-end. I have an eye for design and enjoy creating visually appealing websites. I’ve studied to become a web developer for the past 2 years. And additionally, I have 6 years of experience creating graphics and layouts with the Adobe Suite. I’m passionate about development and I love to learn.</p>
+        </div>
+        <div className='about-item image-wrapper'>
+            <Image src="/images/portrait.jpg" alt="Picture of Me" loading='lazy' width={300} height={300}/>
         </div>
     </section>
 }
