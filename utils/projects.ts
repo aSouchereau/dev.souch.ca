f89d0285c417b20972741ee1c899934c1cba6d99
alexsouchereau <W0632604@myscc.ca>
2023-06-05 19:38:30 -0400

    Reordered projects

--- utils/projects.ts
+++ utils/projects.ts
@@ -23,26 +23,26 @@ export const projects : Project[] = [
         ]
     },
     {
-        title: "Linkboard",
-        slug: "linkboard",
-        subtitle: "Self Hosted Personal Dashboard",
-        description: "Linkboard is a self-hosted homepage for homelabbers and their users. It’s built in Laravel, and incorporates user authentication, a permissions system, a link sharing system, and custom themes.",
-        repoLink: 'https://github.com/aSouchereau/linkboard',
-        video: "/videos/linkboard-demo.mp4",
-        tags: [
-            "Fullstack", "Laravel", "Bootstrap", "Docker", "Open Source"
-        ]
-    },
-    {
         title: "MC Route Manager",
         slug: "mc-route-manager",
-        subtitle: "Client Connection Router for Minecraft Servers",
+        subtitle: "W.I.P. - Client Connection Router for Minecraft Servers",
         description: "MC Route Manager is a management system for a 3rd party tcp router for minecraft client connections. Built in Laravel, the system makes API calls to the router to manage routes. It adds extra functionality like persistent routes, route groups, garbage collection, toggleable and editable routes.",
         repoLink: 'https://github.com/aSouchereau/mcroute-manager',
         demoLink: 'https://mcrouter-demo.alexsouch.com/',
         video: "/videos/mcrouter-demo.mp4",
         tags: [
             "Fullstack", "Laravel", "Bootstrap", "Docker"
+        ]
+    },
+    {
+        title: "Linkboard",
+        slug: "linkboard",
+        subtitle: "W.I.P. - Self Hosted Personal Dashboard",
+        description: "Linkboard is a self-hosted homepage for homelabbers and their users. It’s built in Laravel, and incorporates user authentication, a permissions system, a link sharing system, and custom themes.",
+        repoLink: 'https://github.com/aSouchereau/linkboard',
+        video: "/videos/linkboard-demo.mp4",
+        tags: [
+            "Fullstack", "Laravel", "Bootstrap", "Docker", "Open Source"
         ]
     }
 ]
