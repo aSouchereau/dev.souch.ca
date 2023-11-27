f235e4b324f865c7aff67d7460a98ddc604eb492
aSouchereau <alex@souch.ca>
2023-11-27 12:29:19 -0500

    Updated about me section

--- components/AboutSectionComponent.tsx
+++ components/AboutSectionComponent.tsx
@@ -4,7 +4,7 @@ export default function AboutSection() {
     return <section id='about'>
         <div className='about-item intro-paragraph'>
             <h2 className='light'>Hey, I’m Alex. </h2>
-            <p className='light'>I'm a front-end focused full stack developer with 2 years of dedicated study in web development. I'm passionate about learning and creating visually appealing sites and apps.</p>
+            <p className='light'>I'm a Full Stack Web Developer, trying to enter the industry. I’m excited to gain valuable experience and find my specialty. I find joy in every step of the development process, from designing user interfaces, to planning and problem-solving, or implementing functionality.</p>
         </div>
         <div className='about-item image-wrapper'>
             <Image src="/images/portrait.jpg" alt="Picture of Me" loading='lazy' width={250} height={250}/>
