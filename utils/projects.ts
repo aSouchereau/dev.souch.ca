189a31da3ef7fa6807e71f44accaa8c9194fdceb
aSouchereau <alex@souch.ca>
2023-11-27 11:39:20 -0500

    Made description optional

--- utils/projects.ts
+++ utils/projects.ts
@@ -3,7 +3,7 @@ export type Project = {
     slug: string,
     subtitle: string,
     summary: string,
-    description: string[],
+    description?: string[],
     demoLink?: string,
     repoLink: string,
     video?: string,
@@ -29,11 +29,8 @@ export const projects : Project[] = [
     {
         title: "MC Route Manager",
         slug: "mc-route-manager",
-        subtitle: "W.I.P. - Client Connection Router for Minecraft Servers",
+        subtitle: "Client Connection Router for Minecraft Servers",
         summary: "MC Route Manager is a management system for a 3rd party tcp router for minecraft client connections. Built in Laravel, the system makes API calls to the router to manage routes. It adds extra functionality like persistent routes, route groups, garbage collection, toggleable and editable routes.",
-        description: [
-
-        ],
         repoLink: 'https://github.com/aSouchereau/mcroute-manager',
         demoLink: 'https://mcrouter-demo.alexsouch.com/',
         video: "/videos/mcrouter-demo.mp4",
@@ -44,15 +41,12 @@ export const projects : Project[] = [
     {
         title: "Linkboard",
         slug: "linkboard",
-        subtitle: "W.I.P. - Self Hosted Personal Dashboard",
+        subtitle: "Self Hosted Personal Dashboard",
         summary: "Linkboard is a self-hosted homepage for homelabbers and their users. It’s built in Laravel, and incorporates user authentication, a permissions system, a link sharing system, and custom themes.",
-        description: [
-
-        ],
         repoLink: 'https://github.com/aSouchereau/linkboard',
         video: "/videos/linkboard-demo.mp4",
         tags: [
-            "Fullstack", "Laravel", "Bootstrap", "Docker", "Open Source"
+            "Fullstack", "Laravel", "Bootstrap", "Docker"
         ]
     }
 ]
